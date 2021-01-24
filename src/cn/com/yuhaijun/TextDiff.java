a1335f5da9b18203cd2c8e668ec32a97947ef013
Yu Haijun <yuhaijun83.dev@gmail.com>
2021-01-24 15:06:53 +0900

    更新

--- src/cn/com/yuhaijun/TextDiff.java
+++ src/cn/com/yuhaijun/TextDiff.java
@@ -53,7 +53,6 @@ public class TextDiff {
 	private static void fileChk(String[] args) throws IOException {
 		
 		List<String> lstDiffFile = new ArrayList<String>();
-		lstDiffFile.add("ファイル内容一致していないファイルの一覧：");
 
 		String strOldPath = args[0];
 		String strNewPath = args[1];
@@ -82,13 +81,7 @@ public class TextDiff {
 
 		int iOldFile = mapOld.size();
 		int iNewFile = mapNew.size();
-		if (iOldFile != iNewFile) {
-			System.out.println("両方のファイル個数が違う！");
-			System.out.println(strOldPath + " =>：" + iOldFile);
-			System.out.println(strNewPath + " =>：" + iNewFile);
-			exit();
-		}
-
+		
 		if (iOldFile == 0) {
 			System.out.println(strOldPath + " =>：ファイルが見つかりません！");
 			exit();
@@ -98,83 +91,120 @@ public class TextDiff {
 			exit();
 		}
 		///////////////////////////////////////////////////////////////////////////////////////////
-		StringBuilder sbOldFileNameList = new StringBuilder();
-		StringBuilder sbNewFileNameList = new StringBuilder();
+		List<String> lstOldFileName = new ArrayList<String>();
+		List<String> lstNewFileName = new ArrayList<String>();
+		Map<String, String> mapAllFileName = new HashMap<String, String>();
 		
+		String strKey = null;
 		for (Map.Entry<String, List<String>> entryOld : mapOld.entrySet()) {
-			sbOldFileNameList.append(entryOld.getKey());			
+			strKey = entryOld.getKey();
+			lstOldFileName.add(strKey);
+			mapAllFileName.put(strKey, null);
 		}
-		for (Map.Entry<String, List<String>> entryNew : mapOld.entrySet()) {
-			sbNewFileNameList.append(entryNew.getKey());			
+		for (Map.Entry<String, List<String>> entryNew : mapNew.entrySet()) {
+			strKey = entryNew.getKey();
+			lstNewFileName.add(strKey);
+			mapAllFileName.put(strKey, null);
 		}
-		
-		if (!(sbOldFileNameList.toString()).equals(sbNewFileNameList.toString())) {
-			System.out.println("両方のファイル名が一致していない！");
-			System.out.println(strOldPath + " => FileName All Size ：" + sbOldFileNameList.toString().length());
-			System.out.println(strNewPath + " => FileName All Size ：" + sbNewFileNameList.toString().length());
-			exit();
+
+		List<String> lstDiffFileName = new ArrayList<String>();
+		List<String> lstNotFoundOldFileName = new ArrayList<String>();
+		List<String> lstNotFoundNewFileName = new ArrayList<String>();
+		for (Map.Entry<String, String> entry : mapAllFileName.entrySet()) {
+			strKey = entry.getKey();
+			if (lstOldFileName.contains(strKey) && lstNewFileName.contains(strKey)) {
+				lstDiffFileName.add(strKey);
+			} else {
+				if (!lstOldFileName.contains(strKey)) {
+					lstNotFoundOldFileName.add(strKey);
+				}
+				if (!lstNewFileName.contains(strKey)) {
+					lstNotFoundNewFileName.add(strKey);
+				}
+			}
 		}
 		/////////////////////////////////////////////////////////////////////////////////////////
-
-		String oldFileName = null;
 		List<String> oldLstFileInfo = null;
 		int oldLstCount = 0;
 		String oldFileInfo = null;
 
-		String newFileName = null;
 		List<String> newLstFileInfo = null;
 		int newLstCount = 0;
 		String newFileInfo = null;
 		
-		for (Map.Entry<String, List<String>> entryOld : mapOld.entrySet()) {
-			oldFileName = entryOld.getKey();
-			oldLstFileInfo = entryOld.getValue();
+		for (String fileName: lstDiffFileName) {
 			
-			for (Map.Entry<String, List<String>> entryNew : mapNew.entrySet()) {
-				newFileName = entryNew.getKey();
-				newLstFileInfo = entryNew.getValue();
-				
-				if (oldFileName.equals(newFileName)) {
-					oldLstCount = oldLstFileInfo.size();
-					newLstCount = newLstFileInfo.size();
-					
-					if (oldLstCount != newLstCount) {
-						lstDiffFile.add(oldFileName);
-						break;
-					} else {
+			oldLstFileInfo = mapOld.get(fileName);
+			newLstFileInfo = mapNew.get(fileName);
 
-						for (int i = 0; i < oldLstCount; i++) {
-							if (i == 4) {
-								// ファイルごと、5行目を差分対象外にする　TODO
-								continue;
-							}
-							oldFileInfo = oldLstFileInfo.get(i);
-							newFileInfo = newLstFileInfo.get(i);
-							
-							if (!oldFileInfo.equals(newFileInfo)) {
-								lstDiffFile.add(oldFileName);
-								break;
-							}
-						}
+			oldLstCount = oldLstFileInfo.size();
+			newLstCount = newLstFileInfo.size();
+			
+			if (oldLstCount != newLstCount) {
+				lstDiffFile.add(fileName);
+				break;
+			} else {
+				for (int i = 0; i < oldLstCount; i++) {
+					if (i == 4) {
+						// ファイルごと、5行目を差分対象外にする　TODO
+						continue;
 					}
-					mapNew.remove(newFileName, newLstFileInfo);
-					break;
+					oldFileInfo = oldLstFileInfo.get(i);
+					newFileInfo = newLstFileInfo.get(i);
+					
+					if (!oldFileInfo.equals(newFileInfo)) {
+						lstDiffFile.add(fileName);
+						break;
+					}
 				}
 			}
 		}
+		/////////////////////////////////////////////////////////////////////////////////////////
+		String fileName = "C:\\Windows\\Temp\\TextDiff.txt";
+		File file = new File(fileName);
+		if (file.exists() && file.isFile()) {
+			file.delete();
+		}
 		
-		if (lstDiffFile.size() > 1) {
-			String fileName = "C:\\Windows\\Temp\\TextDiff.txt";
-			File file = new File(fileName);
-			if (file.exists() && file.isFile()) {
-				file.delete();
-			}
-			toWriteFile(fileName, lstDiffFile);
-			Runtime.getRuntime().exec("notepad.exe " + fileName);
+		toWriteFile(fileName, lstNotFoundOldFileName, lstNotFoundNewFileName, lstDiffFile);
+		Runtime.getRuntime().exec("notepad.exe " + fileName);
+
+		if (lstDiffFile.size() > 0) {
+
+
 		} else {
-			System.out.println("両方のファイル内容が同じです！");
+			System.out.println("両方のファイルリストとファイル内容が同じです！");
 		}
 	}
+	
+//	private static int getMaxLength(List<String> lstNotFoundOldFileName, List<String> lstNotFoundNewFileName) {
+//		int iRet = 0;
+//		int iTemp = 0;
+//		
+//		int iOld = 0;
+//		for (String name : lstNotFoundOldFileName) {
+//			iTemp = name.length();
+//			if (iTemp > iOld) {
+//				iOld = iTemp;
+//			}
+//		}
+//		
+//		int iNew = 0;
+//		for (String name : lstNotFoundOldFileName) {
+//			iTemp = name.length();
+//			if (iTemp > iNew) {
+//				iNew = iTemp;
+//			}
+//		}
+//		
+//		if (iOld > iNew) {
+//			iRet = iOld;
+//		} else {
+//			iRet = iNew;
+//		}
+//		
+//		return iRet;
+//	}
 
 	private static Map<String, List<String>> readFolder(File filePath) throws IOException {
 		Map<String, List<String>> mapRet = new HashMap<String, List<String>>();
@@ -218,14 +248,45 @@ public class TextDiff {
 		return lstLine;
 	}
 
-	private static void toWriteFile(String fileName, List<String> info) throws IOException {
+	private static void toWriteFile(String fileName, List<String> lstNotFoundOldFileName, 
+			List<String> lstNotFoundNewFileName, List<String> lstDiffFile) throws IOException {
 		FileWriter writer = null;
         try {
             writer = new FileWriter(fileName, true);
-            for (String content : info) {
-                writer.write(content);
+
+            if (lstNotFoundOldFileName.size() > 0) {
+                writer.write("下記ファイル名一覧は、フォルダ２に存在しているが、ファイル１に見つかりません！");
+                writer.write("\r\n");
+                for (String content : lstNotFoundOldFileName) {
+                    writer.write(content);
+                    writer.write("\r\n");
+                }
+                writer.write("---------------------------------------------------------------------------------------------------------");
                 writer.write("\r\n");
             }
+
+            if (lstNotFoundNewFileName.size() > 0) {
+                writer.write("下記ファイル名一覧は、フォルダ１に存在しているが、ファイル２に見つかりません！");
+                writer.write("\r\n");
+                for (String content : lstNotFoundNewFileName) {
+                    writer.write(content);
+                    writer.write("\r\n");
+                }
+                writer.write("---------------------------------------------------------------------------------------------------------");
+                writer.write("\r\n");
+            }
+            
+            if (lstDiffFile.size() > 0) {
+                writer.write("下記ファイル名のファイル内容は両方が違う！");
+                writer.write("\r\n");
+                for (String content : lstDiffFile) {
+                    writer.write(content);
+                    writer.write("\r\n");
+                }
+                writer.write("---------------------------------------------------------------------------------------------------------");
+                writer.write("\r\n");
+            }
+            
             writer.close();
         } catch (IOException e) {
 			throw new IOException(e);
